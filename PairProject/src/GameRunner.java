ec3cb3d4cd48f401128d1a61274802a07dfe34c9
faculty <faculty@10.129.224.22>
2023-03-24 09:37:35 -0400

    GAME RUNNER

---
+++ PairProject/src/GameRunner.java
@@ -0,0 +1,13 @@
+public class GameRunner {
+	public GameRunner() {
+		
+	}
+	
+	public static void main(String[] args) {
+		GameRunner game = new GameRunner();
+		System.out.println("yo");
+		System.out.println("joel2");
+		System.out.println("dia2");
+		System.out.println("mard2");
+	}
+}
