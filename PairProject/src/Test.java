4af873712062d6f1dce1167745ef28f0463f4295
teky1 <joelchem@outlook.com>
2023-03-24 09:30:22 -0400

    ok

--- PairProject/src/Test.java
+++ PairProject/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("yo");
-		System.out.println("joel");
+		System.out.println("joel2");
 		System.out.println("dia");
 		System.out.println("mard");
 		
