ce601cbb9111f08518926d95887838db3b6cc149
faculty <faculty@10.129.224.22>
2023-03-24 10:08:49 -0400

    blaeiugh

--- PairProject/src/GamePanel.java
+++ PairProject/src/GamePanel.java
@@ -1,8 +1,12 @@
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class GamePanel extends JPanel{
 	public GamePanel() {
 		
 	}
 	
+	public void paintComponent(Graphics g) {
+		
+	}
 }
