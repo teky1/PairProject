2b23e3c4ffb43ea5a5ed34ef4393564a08d63dba
teky1 <joelchem@outlook.com>
2023-04-17 09:47:18 -0400

    VECTOR

--- PairProject/src/Vector.java
+++ PairProject/src/Vector.java
@@ -1,4 +1,16 @@
+import java.awt.Point;
 
-public class Vector {
-
+// Represents a 2D Vector
+public class Vector extends Point.Double{
+	// takes in posX and posY and passes it into superclass
+	Vector(double posX, double posY) {
+		super(posX, posY);
+	}
+	Vector(int posX, int posY) {
+		super((double)posX, (double)posY);
+	}
+	// adds two vectors together returning the result
+	public Vector add(Vector other) {
+		return new Vector(getX()+other.getX(), getY()+other.getY());
+	}
 }
