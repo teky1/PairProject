1a9604ee942ffb4f4f47dccd356eca964284bb2e
nimeister <nimeister@HN-1029-66308.hunter.local>
2023-05-04 10:05:40 -0400

    did it work

--- PairProject/src/MainScreen.java
+++ PairProject/src/MainScreen.java
@@ -1,11 +1,34 @@
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class MainScreen extends JPanel {
+	JFrame frame;
+	JPanel panel;
+	JLabel title, logo;
+	JButton playButton;
 	
 	public MainScreen() {
-		
+		this.frame = new JFrame("Main Screen");
+		this.panel = new JPanel();
+		panel.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.setPreferredSize(new Dimension(800, 700));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setVisible(true);
+		playButton = new JButton(new ImageIcon(""));
+		playButton.setBounds(10,10,110,60);
+		panel.add(playButton);
+
+		playButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//switch screen or something idk
+			}
+		});
 	}
-	
-	
-	
+	public static void Main(String args[]) {
+		new MainScreen();
+	}
 }
