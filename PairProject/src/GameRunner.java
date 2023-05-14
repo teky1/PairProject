1a761121693d3d42cf5ab8ddd814e949463b1ea5
Teky1 <officialteky@gmail.com>
2023-05-14 18:39:42 -0400

    sfd

--- PairProject/src/GameRunner.java
+++ PairProject/src/GameRunner.java
@@ -13,26 +13,25 @@ public class GameRunner implements ActionListener {
 	private boolean started;
 	private int currentLevel;
 	private int maxLevel;
-	
+
 	private long lastFrame;
 	private long timeStarted;
-	
-	
+
 	public GameRunner() {
 		game = new Game();
 		gamePanel = new GamePanel(game, this);
 		menu = new MainScreen(this);
-		
+
 		maxLevel = 3;
-		started = false;	
+		started = false;
 	}
-	
+
 	public boolean getStarted() {
 		return started;
 	}
-	
+
 	public void start(boolean isRandom) {
-		if(isRandom) {
+		if (isRandom) {
 			currentLevel = 0;
 		} else {
 			currentLevel = 1;
@@ -40,97 +39,88 @@ public class GameRunner implements ActionListener {
 		setupGameloop();
 		startGameloop();
 	}
-	
+
 	public void startGameloop() {
-    	started = true;
-    	timeStarted = System.currentTimeMillis();
-    	lastFrame = System.currentTimeMillis()%1000000;
-    	timer = new Timer(1, this);
-    	timer.setInitialDelay(0);
-    	timer.start();
-    }
-	
+		started = true;
+		timeStarted = System.currentTimeMillis();
+		lastFrame = System.currentTimeMillis() % 1000000;
+		timer = new Timer(1, this);
+		timer.setInitialDelay(0);
+		timer.start();
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		calculateFrame();
 		gamePanel.renderFrame();
 	}
-	
-	public void setupGameloop(){
-	       
-	       /*
-	        * Include code that intiializes stuff in Game with relevent information
-	        * 
-	        * */
-			game.loadLevel(0);
-		
-	       
-	       GamePanel.startFrame(gamePanel);
-	       lastFrame = System.currentTimeMillis()%1000000;
-	       calculateFrame();
-	       gamePanel.renderFrame();
+
+	public void setupGameloop() {
+
+		/*
+		 * Include code that intiializes stuff in Game with relevent information
+		 * 
+		 */
+		game.loadLevel(0);
+
+		GamePanel.startFrame(gamePanel);
+		lastFrame = System.currentTimeMillis() % 1000000;
+		calculateFrame();
+		gamePanel.renderFrame();
 	}
-	
+
 	public void calculateFrame() {
-		
+
 		Vector mousePos = new Vector(gamePanel.getMousePosition());
 		Platform platform = game.getPlatform();
 		platform.setPos(new Vector(mousePos.getX(), platform.getPos().getY()));
-		
-		
-		
-		
-		
-		long currTime = System.currentTimeMillis()%1000000;
-    double timeDelta = (double)(currTime - lastFrame)/1000.;
 
-    boolean bricksActive = false;
-    boolean ballsActive = false;
+		long currTime = System.currentTimeMillis() % 1000000;
+		double timeDelta = (double) (currTime - lastFrame) / 1000.;
 
-    ArrayList<Ball> balls = game.getBalls();
-		for(Ball ball : balls) {
+		boolean bricksActive = false;
+		boolean ballsActive = false;
+
+		ArrayList<Ball> balls = game.getBalls();
+		for (Ball ball : balls) {
 			platform.handleCollisions(ball);
 			ball.update(timeDelta);
-			
-			if(ball.isActive()) {
+
+			if (ball.isActive()) {
 				ballsActive = true;
 			}
-			
-			for(Brick brick : game.getLevel().getBricks()) {
+
+			for (Brick brick : game.getLevel().getBricks()) {
 				brick.handleCollisions(ball);
-				if(brick.isActive()) {
+				if (brick.isActive()) {
 					bricksActive = true;
 				}
 			}
 		}
-    	
-    ArrayList<PowerUp> powerups = game.getPowerups();
-		for(PowerUp powerup : powerups) {
+
+		ArrayList<PowerUp> powerups = game.getPowerups();
+		for (PowerUp powerup : powerups) {
 			powerup.update(timeDelta);
 		}
-		
-		if(!bricksActive) {
+
+		if (!bricksActive) {
 			game.reset();
-			if(currentLevel > 0 && currentLevel < maxLevel) {
+			if (currentLevel > 0 && currentLevel < maxLevel) {
 				currentLevel++;
 				game.loadLevel(currentLevel);
 			} else if (currentLevel == 0) {
 				game.loadLevel(0);
 			}
 		}
-		if(!ballsActive) {
+		if (!ballsActive) {
 			System.out.println("lose");
 		}
-    	
-	
-    game.getBall().update(timeDelta);
-    for(int i=0; i<game.getLevel().getAsteroids().size(); i++) {
-      game.getLevel().getAsteroids().get(i).update(timeDelta);
-    }
-    lastFrame = currTime;
+
+		for (int i = 0; i < game.getLevel().getAsteroids().size(); i++) {
+			game.getLevel().getAsteroids().get(i).update(timeDelta);
+		}
+		lastFrame = currTime;
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		GameRunner game = new GameRunner();
 //		game.setupGameloop();
