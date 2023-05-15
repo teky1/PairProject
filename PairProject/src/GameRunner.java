ed007874eaffef7b9723839547cb2cc218f7cfd6
d-i-a-395 <dia.nawathe@gmail.com>
2023-05-15 00:16:00 -0400

    oops

--- PairProject/src/GameRunner.java
+++ PairProject/src/GameRunner.java
@@ -140,8 +140,8 @@ public class GameRunner implements ActionListener {
 
 	public static void main(String[] args) {
 		GameRunner game = new GameRunner();
-		game.setupGameloop();
-		game.startGameloop();
+//		game.setupGameloop();
+//		game.startGameloop();
 	}
 
 }
